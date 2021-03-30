748c9b67a02dcda0392417ec8dc4ba55febc31fc
Ubuntu <ubuntu@ip-172-31-51-119.ec2.internal>
2021-03-30 20:57:55 +0000

    updated pull script

--- pull.sh
+++ pull.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-current_date=`date +"%Y-%m-%d %H:%M"`
-
+echo `date +"%Y-%m-%d %H:%M"` >> /home/ubuntu/pull_script.log
 cd /home/ubuntu/S21-4910-Project/
-git_output=`git pull`
-
-log_output="${current_date} ${git_output}"
-
-echo "$log_output" >> /home/ubuntu/pull_script.log
+echo `git pull` >> /home/ubuntu/pull_script.log
