41163c55752f0411c9108bda2e4fc2939fa7f6f1
Ubuntu <ubuntu@ip-172-31-51-119.ec2.internal>
2021-03-30 21:53:05 +0000

    deleted script

--- pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-current_date=`date +"%d-%b-%Y"`
-file_location="/home/ubuntu/pull_log/${current_date}.log"
-
-echo `date +"%d-%b-%Y %H:%M"` >> $file_location
-cd /home/ubuntu/S21-4910-Project/
-echo `git pull` >> $file_location
